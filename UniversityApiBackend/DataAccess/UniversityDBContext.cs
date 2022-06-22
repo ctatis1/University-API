ca907b46ec60f2ffca276dc76a2d149390058f33
Camilo <52603058+ctatis1@users.noreply.github.com>
2022-06-21 23:30:20 -0500

    Agregar archivos de proyecto.

---
+++ UniversityApiBackend/DataAccess/UniversityDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UniversityApiBackend.Models.DataModels;
+
+namespace UniversityApiBackend.DataAccess
+{
+    public class UniversityDBContext: DbContext
+    {  
+        //constructor
+        public UniversityDBContext(DbContextOptions<UniversityDBContext> options):base(options)
+        {
+
+        }
+
+        //Add DBsets (tables of DB)
+        public DbSet<User>? Users { get; set; }
+    }
+}
